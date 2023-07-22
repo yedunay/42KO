61217390ea7396a0c71d6782ea6c6e071ac2f853
Yunus Emre Dunay <yedun4y@gmail.com>
2023-07-22 10:57:47 +0300

    a

--- exam/ft_atoi.c
+++ exam/ft_atoi.c
@@ -1,34 +1,38 @@
 #include <unistd.h>
 #include <stdio.h>
-int	ft_atoi(const char *str)
+int ft_atoi(char *str)
 {
-	int	i;
-	int	result;
-	int	sign;
+	int i;
+	int current;
+	int sign;
 
-	result = 0;
-	sign = 1;
 	i = 0;
-
-	while(str[i] != '\0')
+	current = 0;
+	sign = 1;
+	if(str[0] == '\0')
+	{
+		return 0;
+	}
+	if(str[0] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	while (str[i] != '\0')
 	{
 		if(str[i] >= '0' && str[i] <= '9')
 		{
-			result *= 10;
-			result += str[i] - '0';
+			current *= 10;
+			current += str[i] - '0';
 		}
-		if(str[i] == '-')
-			sign *= -1;
+		else
+			break;
 		i++;
 	}
-
-	return (result * sign);
+	return (current * sign);
 }
-
 int	main (void)
 {
-	char str[] = "   --++3YED456";
-	ft_atoi(str);
+	char str[] = "-3YED456";
 	printf("%d",ft_atoi(str));
-
 }
